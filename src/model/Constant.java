4bc132648985d42dbf30ad233032091a73230277
utopiabeam <utopiabeam@gmail.com>
2018-11-12 22:41:53 +0700

    Add Constant class

---
+++ src/model/Constant.java
@@ -0,0 +1,10 @@
+package model;
+
+public final class Constant {
+
+	public static final int WINDOW_HEIGHT = 600;
+	public static final int WINDOW_WIDTH = 900;
+	
+	public static final int INVENTORY_SIZE = 5;
+	
+}
