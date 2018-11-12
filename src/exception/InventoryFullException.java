d269e46fd8872f28012de4e3159c2fde6d5a41a0
utopiabeam <utopiabeam@gmail.com>
2018-11-12 22:42:54 +0700

    Add Player class & InventoryFullException

---
+++ src/exception/InventoryFullException.java
@@ -0,0 +1,22 @@
+package exception;
+
+public class InventoryFullException extends Exception {
+
+	private static final long serialVersionUID = -164985566829121492L;
+
+	public InventoryFullException() {
+		super();
+	}
+	
+	public InventoryFullException(String message) {
+		super(message);
+	}
+	
+	public InventoryFullException(Throwable cause) {
+		super(cause);
+	}
+	
+	public InventoryFullException(String message, Throwable cause) {
+		super(message, cause);
+	}
+}
