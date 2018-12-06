5fc20d5ff5e05e3ba62caed4ebe3d3fb3a5d6469
utopiabeam <utopiabeam@gmail.com>
2018-12-06 21:44:51 +0700

    Implement AttackItem

--- src/model/item/AttackItem.java
+++ src/model/item/AttackItem.java
@@ -1,8 +1,14 @@
 package model.item;
 
+import java.util.List;
+
+import controller.GameManager;
 import javafx.scene.image.Image;
+import model.npc.NPC;
 
 public class AttackItem extends Item {
+	
+	public static final int INFINITE = 1<<30;
 	
 	public AttackItem(Image image, int maxCount) {
 		super("Attack Item", maxCount, image);
@@ -10,6 +16,11 @@ public class AttackItem extends Item {
 
 	@Override
 	public boolean activate() {
+		List<NPC> npcs = GameManager.getInstance().getCurrentMap().getListNPC();
+		
+		for (NPC npc: npcs) {
+			npc.takeDamge(INFINITE);
+		}
 		return true;
 	}
 	
