2bd70cdfd92be25fe3b9ed41cd8b0637505a4851
utopiabeam <utopiabeam@gmail.com>
2018-12-08 02:01:20 +0700

    Fix CC always applied

--- src/model/item/CCItem.java
+++ src/model/item/CCItem.java
@@ -5,59 +5,52 @@ import java.util.List;
 import constants.CCType;
 import constants.Images;
 import controller.GameManager;
-import model.IUpdatable;
 import model.npc.NPC;
 import model.player.Player;
 
-public class CCItem extends Item implements IUpdatable {
+public class CCItem extends Item {
 
-	private int duration;
+	public static final int duration = 90;
 
 	public CCItem() {
 		super("Random CC Item", 10, Images.cc);
-		this.duration = 120;
 		this.setImage(Images.cc);
 	}
 	
 	public CCItem(double posX, double posY) {
 		super("Random CC Item", 10, Images.cc, posX, posY);
-		this.duration = 120;
 		this.setImage(Images.cc);
 	}
 
 	@Override
 	public boolean activate() {
+		Player player = GameManager.getInstance().getPlayer();
+		if (player.isCCUsed()) {
+			return false;
+		}
+		
+		player.setCCUsed(true);
+		
 		double random = Math.random();
 		
 		List<NPC> npcs = GameManager.getInstance().getCurrentMap().getListNPC();
 		if (random <= 0.2) {
+//			System.out.println("NPCs are stunned");
 			for (NPC npc: npcs) {
 				npc.setStatus(CCType.STUN);
 			}
-		} else if (random <= 0.3) {
+		} else if (random <= 0.5) {
+//			System.out.println("NPCs are slowed");
 			for (NPC npc: npcs) {
 				npc.setStatus(CCType.SLOW);
 			}
 		} else {
+//			System.out.println("NPCs are silenced");
 			for (NPC npc: npcs) {
 				npc.setStatus(CCType.SILENCE);
 			}
 		}
 		return true;
-	}
-	
-	@Override
-	public void update() {
-		if (--duration == 0) {
-			Player player = GameManager.getInstance().getPlayer();
-			player.setStatus(CCType.NONE);
-		}
-	}
-
-	// Getters & Setters
-	
-	public int getDuration() {
-		return duration;
 	}
 
 }
