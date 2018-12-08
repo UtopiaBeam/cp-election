feb0da8ef3341921d1d2828caa862e88975db9f8
UtopiaBeam <utopiabeam@gmail.com>
2018-12-09 02:22:16 +0700

    Remove duplicate attributes & methods

--- src/model/npc/Boss.java
+++ src/model/npc/Boss.java
@@ -8,31 +8,14 @@ import skill.*;
 public class Boss extends Character {
 	
 	private Skill[] skills = { new ShockSkill(), new PodiumSkill() };
-	private int skillTick = 0;
-	private int skillCoolDown = 90;
-	private boolean isUsingSkill = false;
 
 	public Boss(double posX, double posY) {
 		super(posX, posY, "Prayut", Images.prayutL, Images.prayutR, 10000, 100, 1000, 100, 120);
 	}
-
-	public void resetSkillTick() {
-		skillTick = 0;
-	}
-	
-	public void addSkillTick() {
-		if (skillTick < skillCoolDown) {
-			skillTick++;
-		}
-		if (skillTick == skillCoolDown) {
-			resetSkillTick();
-			setUsingSkill(false);
-		}
-	}
 	
 	@Override
 	public void attack() throws CannotAttackException {
-		if (isUsingSkill()) {
+		if (isAttacking()) {
 			throw new CannotAttackException();
 		}
 		
@@ -46,31 +29,13 @@ public class Boss extends Character {
 
 	@Override
 	public void update() {
-		if (isUsingSkill()) {
-			addSkillTick();
-		}
+		addAttackTick();
 	}
 	
 	// Getters & Setters
 
 	public Skill[] getSkills() {
 		return skills;
-	}
-
-	public int getSkillCoolDown() {
-		return skillCoolDown;
-	}
-
-	public boolean isUsingSkill() {
-		return isUsingSkill;
-	}
-
-	public void setSkillCoolDown(int skillCoolDown) {
-		this.skillCoolDown = skillCoolDown;
-	}
-
-	public void setUsingSkill(boolean isUsingSkill) {
-		this.isUsingSkill = isUsingSkill;
 	}
 	
 }
