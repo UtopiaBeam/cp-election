5be0918afea78a0e524b99e76b2aa9e5de4382c6
UtopiaBeam <utopiabeam@gmail.com>
2018-12-09 01:40:37 +0700

    Change AttackItem behaviour

--- src/model/item/AttackItem.java
+++ src/model/item/AttackItem.java
@@ -2,29 +2,36 @@ package model.item;
 
 import java.util.List;
 
-import constants.Constants;
 import constants.Images;
 import controller.GameManager;
 import model.npc.NPC;
 
 public class AttackItem extends Item {
 	
+	private final int minDamage = 100;
+	private final int maxDamage = 150;
+	
 	public AttackItem() {
-		super("Attack Item", 2, Images.attack);
+		super("Attack Item", 5, Images.attack);
 		this.setImage(Images.attack);
 	}
 	
 	public AttackItem(double posX, double posY) {
-		super("Attack Item", 2, Images.attack, posX, posY);
+		super("Attack Item", 5, Images.attack, posX, posY);
 		this.setImage(Images.attack);
+	}
+	
+	private int getDamage() {
+		return minDamage + (int) (Math.random() * (maxDamage - minDamage + 1));
 	}
 
 	@Override
 	public boolean activate() {
 		List<NPC> npcs = GameManager.getInstance().getCurrentMap().getListNPC();
+		int damage = getDamage();
 		
 		for (NPC npc: npcs) {
-			npc.takeDamage(Constants.INFINITE);
+			npc.takeDamage(damage);
 		}
 		return true;
 	}
