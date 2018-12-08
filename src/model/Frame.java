d530be03502c1f1f18fb4314496e113f13860de8
utopiabeam <utopiabeam@gmail.com>
2018-12-08 21:18:51 +0700

    Remove unused method

--- src/model/Frame.java
+++ src/model/Frame.java
@@ -21,11 +21,6 @@ public class Frame {
 			(posY < f.posY + f.height) && (height + posY > f.posY);
 	}
 	
-	public boolean isInWindow() {
-		return (posX >= 0) && (posX + width < GameScene.WINDOW_WIDTH) &&
-				(posY >= 0) && (posY + height < GameScene.WINDOW_HEIGHT);
-	}
-	
 	// Getters & Setters
 
 	public double getPosX() {
