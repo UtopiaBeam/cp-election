be304b100d1e3f85d86bd833c46714a08f604129
CeylonT <nitikub@gmail.com>
2018-12-09 18:45:15 +0700

    Trim bgm

--- src/constants/Sounds.java
+++ src/constants/Sounds.java
@@ -4,5 +4,5 @@ import javafx.scene.media.AudioClip;
 
 public class Sounds {
 	public static final AudioClip startbgm = new AudioClip(ClassLoader.getSystemResource("audio/startscenebgm.m4a").toString());
-	public static final AudioClip bgm = new AudioClip(ClassLoader.getSystemResource("audio/gamescenebgm.mp3").toString());
+	public static final AudioClip bgm = new AudioClip(ClassLoader.getSystemResource("audio/gamescenebgm.m4a").toString());
 }
