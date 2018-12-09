c28698a5ae0dc87c9e4705fa3004ece3dd32b448
UtopiaBeam <utopiabeam@gmail.com>
2018-12-09 16:36:53 +0700

    Generate podium when use skill

--- src/skill/PodiumSkill.java
+++ src/skill/PodiumSkill.java
@@ -1,5 +1,9 @@
 package skill;
 
+import controller.GameManager;
+import model.effect.Podium;
+import ui.GameScene;
+
 public class PodiumSkill extends Skill {
 
 	public PodiumSkill() {
@@ -11,6 +15,11 @@ public class PodiumSkill extends Skill {
 		if (isCoolingDown()) {
 			return;
 		}
+		
+		double x = (Math.random() <= 0.5) ? 0 : GameScene.WINDOW_WIDTH;
+		double y = GameManager.getInstance().getPlayer().getPosY();
+		
+		GameManager.getInstance().getCurrentMap().addPodium(new Podium(x, y));
 	}
 	
 }
