83df512d23882fe73bb9ec5448518c8c0866ce05
UtopiaBeam <utopiabeam@gmail.com>
2018-12-10 18:57:08 +0700

    Make boss respawn when died

--- src/model/npc/Prawit.java
+++ src/model/npc/Prawit.java
@@ -1,12 +1,21 @@
 package model.npc;
 
 import constants.Images;
+import controller.GameManager;
 import skill.WatchSkill;
 
 public class Prawit extends Boss {
 
 	public Prawit(double posX, double posY) {
 		super(posX, posY, "Prawit", Images.prawitL, Images.prawitR, 5000, 50, 200, 80, new WatchSkill());
+	}
+	
+	@Override
+	public void dead() {
+		if (!isDead()) {
+			return;
+		}
+		GameManager.getInstance().getCurrentMap().spawnBossPrayutRandom();
 	}
 	
 }
