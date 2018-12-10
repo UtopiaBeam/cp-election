98c7d57388bee6890b66d24f861bd0cfeee25d37
UtopiaBeam <utopiabeam@gmail.com>
2018-12-10 20:49:34 +0700

    Rename field

--- src/ui/HpBar.java
+++ src/ui/HpBar.java
@@ -11,10 +11,10 @@ public class HpBar {
 	
 	public static final double WIDTH = 60;
 	public static final double HEIGHT = 6;
-	private Character entity;
+	private Character character;
 	
-	public HpBar(Character entity) {
-		this.entity = entity;
+	public HpBar(Character character) {
+		this.character = character;
 	}
 	
 	public void render(GraphicsContext gc) {
@@ -22,18 +22,18 @@ public class HpBar {
 		double mapX = GameManager.getInstance().getCurrentMap().getPosX();
 		double mapY = GameManager.getInstance().getCurrentMap().getPosY();
 		
-		double centerX = entity.getPosX() + entity.getWidth() / 2;
+		double centerX = character.getPosX() + character.getWidth() / 2;
 		double startX = centerX - WIDTH / 2;
 		gc.setFill(Color.GRAY);
-		gc.fillRect(startX-mapX, entity.getPosY()+100+HEIGHT-mapY, WIDTH, HEIGHT);
+		gc.fillRect(startX-mapX, character.getPosY()+100+HEIGHT-mapY, WIDTH, HEIGHT);
 		gc.setFill(Color.GREEN.	brighter());
-		gc.fillRect(startX-mapX, entity.getPosY()+100+HEIGHT-mapY, WIDTH * entity.getHp() / entity.getMaxHp(), HEIGHT);
+		gc.fillRect(startX-mapX, character.getPosY()+100+HEIGHT-mapY, WIDTH * character.getHp() / character.getMaxHp(), HEIGHT);
 		gc.setStroke(Color.BLACK);
 		gc.setLineWidth(1);
-		gc.strokeRect(startX-mapX, entity.getPosY()+100+HEIGHT-mapY, WIDTH, HEIGHT);
+		gc.strokeRect(startX-mapX, character.getPosY()+100+HEIGHT-mapY, WIDTH, HEIGHT);
 		
-		if (entity instanceof Player && ((Player) entity).isRevivable()) {
-			gc.drawImage(Images.revive, startX-mapX+WIDTH+5, entity.getPosY()+100+HEIGHT-mapY-7, 20, 20);
+		if (character instanceof Player && ((Player) character).isRevivable()) {
+			gc.drawImage(Images.revive, startX-mapX+WIDTH+5, character.getPosY()+100+HEIGHT-mapY-7, 20, 20);
 		}
 	}
 	
